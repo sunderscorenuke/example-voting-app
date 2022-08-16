26a6312ee85795da736c2590c5237606d9aa6a8c
eric egan <36870093+okapetanios@users.noreply.github.com>
2022-08-15 20:50:50 -0700

    Cats Dogs >> Lions Tigers

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Lions")
+option_b = os.getenv('OPTION_B', "Tigers")
 hostname = socket.gethostname()
 version = 'v1'
 
